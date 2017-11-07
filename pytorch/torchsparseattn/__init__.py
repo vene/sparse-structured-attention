c439ccfb23c758d748fc7b270baf61033a8597ed
Vlad Niculae <vlad@vene.ro>
2017-11-06 20:33:04 -0500

    bump dev version

--- pytorch/torchsparseattn/__init__.py
+++ pytorch/torchsparseattn/__init__.py
@@ -2,4 +2,4 @@ from .fused import Fusedmax, FusedProxFunction
 from .oscar import Oscarmax, OscarProxFunction
 from .sparsemax import Sparsemax, SparsemaxFunction
 
-__VERSION__ = '0.1.dev0'
+__VERSION__ = '0.2.dev0'
