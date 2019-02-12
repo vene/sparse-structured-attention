e965b5e059fbe6db696c2e6be089110c674ea102
vene <vlad@vene.ro>
2019-02-12 00:07:32 +0000

    release 0.2

--- pytorch/setup.py
+++ pytorch/setup.py
@@ -18,7 +18,7 @@ extensions = cythonize(extensions)
 
 
 setup(name="torchsparseattn",
-      version="0.2.dev0",
+      version="0.2",
       description="Sparse structured attention mechanisms for pytorch",
       author="Vlad Niculae",
       author_email="vlad@vene.ro",
